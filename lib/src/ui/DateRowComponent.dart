afb53d84733c30fb8d0e04a3b902a555c58a5e2f
KarolisZiukas <kazi@danskebank.lt>
2019-02-13 23:57:42 +0200

    ripple on date picker

--- lib/src/ui/DateRowComponent.dart
+++ lib/src/ui/DateRowComponent.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class DateRowComponent extends StatefulWidget {
-
   final ValueChanged<String> onPressed;
 
   DateRowComponent({this.onPressed});
@@ -13,9 +12,7 @@ class DateRowComponent extends StatefulWidget {
   }
 }
 
-
 class DateRowComponentState extends State<DateRowComponent> {
-
   var formatter = new DateFormat('yyyy-MM-dd');
   DateTime currentDate = DateTime.now();
   String formatted = "Please select date";
@@ -39,41 +36,41 @@ class DateRowComponentState extends State<DateRowComponent> {
       mainAxisAlignment: MainAxisAlignment.start,
       children: <Widget>[
         Flexible(
-          child: Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15.0),
-                color: Color(0xFFFFFFFF),
-                border: Border.all(color: Color(0xFFFFFFFF), width: 2.0)),
-            padding: EdgeInsets.all(16),
-            margin: EdgeInsets.all(16),
-            child: GestureDetector(
-                onTap: () => _selectDate(context),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            'Select shift date',
-                            style: TextStyle(fontSize: 12),
-                          ),
-                          Text(
-                            '$formatted',
-                            style: TextStyle(fontSize: 18),
-                          ),
-                        ],
+          child: InkWell(
+              onTap: () => _selectDate(context),
+              child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15.0),
+                      color: Color(0xFFFFFFFF),
+                      border: Border.all(color: Color(0xFFFFFFFF), width: 2.0)),
+                  padding: EdgeInsets.all(16),
+                  margin: EdgeInsets.all(16),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              'Select shift date',
+                              style: TextStyle(fontSize: 12),
+                            ),
+                            Text(
+                              '$formatted',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    Icon(
-                      Icons.calendar_today,
-                      color: Colors.black,
-                    )
-                  ],
-                )
-            ),
+                      Icon(
+                        Icons.calendar_today,
+                        color: Colors.black,
+                      )
+                    ],
+                  )),
+            splashColor: Colors.black,
           ),
         ),
       ],
