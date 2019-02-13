f6844151207984d8be3e9cc7aa9dd7921a71846a
KarolisZiukas <kazi@danskebank.lt>
2019-02-13 23:18:45 +0200

    callback from date picker

--- lib/src/ui/AddNewShiftActivity.dart
+++ lib/src/ui/AddNewShiftActivity.dart
@@ -1,36 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-
+import 'DateRowComponent.dart';
 class AddNewShiftActivity extends StatefulWidget {
   @override
   AddNewShiftActivityState createState() => AddNewShiftActivityState();
 }
 
-Widget datePickerComponent = Text("Shift date");
-
 class AddNewShiftActivityState extends State<AddNewShiftActivity> {
-  final textEditingController = TextEditingController();
-  var formatter = new DateFormat('yyyy-MM-dd');
-  DateTime currentDate = DateTime.now();
-  String formatted = "Please select date";
 
-  @override
-  void dispose() {
-    // Clean up the controller when the Widget is disposed
-    textEditingController.dispose();
-    super.dispose();
-  }
+  var gotTheDate = "NO DATE";
 
-  Future<Null> _selectDate(BuildContext context) async {
-    final DateTime picked = await showDatePicker(
-        context: context,
-        initialDate: currentDate,
-        firstDate: DateTime(2015, 8),
-        lastDate: DateTime(2101));
-    if (picked != null)
-      setState(() {
-        formatted = DateFormat('yMMMMd').format(picked);
-      });
+  void printDate(String date) {
+    setState(() {
+      gotTheDate = date;
+    });
   }
 
   @override
@@ -44,53 +26,11 @@ class AddNewShiftActivityState extends State<AddNewShiftActivity> {
       ),
       body: Column(
         children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Flexible(
-                child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15.0),
-                      color: Color(0xFFFFFFFF),
-                      border: Border.all(
-                          color: Color(0xFFFFFFFF),
-                          width: 2.0
-                      )
-                  ),
-                  padding: EdgeInsets.all(16),
-                  margin: EdgeInsets.all(16),
-                  child: GestureDetector(
-                          onTap: () => _selectDate(context),
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: <Widget>[
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: <Widget>[
-                                    Text(
-                                        'Select shift date',
-                                      style: TextStyle(
-                                        fontSize: 12
-                                      ),
-                                    ),
-                                    Text('$formatted',
-                                      style: TextStyle(
-                                          fontSize: 18
-                                      ),),
-                                  ],
-                                ),
-                              ),
-                              Icon(
-                                Icons.calendar_today,
-                                color: Colors.black,
-                              )
-                            ],
-                          )),
-                ),
-              ),
-            ],
+            DateRowComponent(
+              onPressed: printDate,
+            ),
+          Text(
+            '$gotTheDate'
           )
         ],
       ),
---
+++ lib/src/ui/DateRowComponent.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class DateRowComponent extends StatefulWidget {
+
+  final ValueChanged<String> onPressed;
+
+  DateRowComponent({this.onPressed});
+
+  @override
+  State createState() {
+    return DateRowComponentState();
+  }
+}
+
+
+class DateRowComponentState extends State<DateRowComponent> {
+
+  var formatter = new DateFormat('yyyy-MM-dd');
+  DateTime currentDate = DateTime.now();
+  String formatted = "Please select date";
+
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: currentDate,
+        firstDate: DateTime(2015, 8),
+        lastDate: DateTime(2101));
+    if (picked != null)
+      setState(() {
+        formatted = DateFormat('yMMMMd').format(picked);
+        widget.onPressed(formatted);
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Flexible(
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                color: Color(0xFFFFFFFF),
+                border: Border.all(color: Color(0xFFFFFFFF), width: 2.0)),
+            padding: EdgeInsets.all(16),
+            margin: EdgeInsets.all(16),
+            child: GestureDetector(
+                onTap: () => _selectDate(context),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Select shift date',
+                            style: TextStyle(fontSize: 12),
+                          ),
+                          Text(
+                            '$formatted',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Icon(
+                      Icons.calendar_today,
+                      color: Colors.black,
+                    )
+                  ],
+                )
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
